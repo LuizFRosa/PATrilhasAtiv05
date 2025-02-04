82e248c109894379d87c503787e919030da22ab4
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-04 09:23:38 -0300

    N-A lista 6.cs

---
+++ N-A lista 6.cs
@@ -0,0 +1,33 @@
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escolha uma opção de exibição:");
+        Console.WriteLine("1 - Data completa");
+        Console.WriteLine("2 - Hora completa");
+        Console.WriteLine("3 - Data e hora");
+
+        int escolha = int.Parse(Console.ReadLine());
+
+        DateTime dataHoraAtual = DateTime.Now;
+
+        switch (escolha)
+        {
+            case 1:
+                Console.WriteLine("Data completa: " + dataHoraAtual.ToString("dddd, dd MMMM yyyy"));
+                break;
+            case 2:
+                Console.WriteLine("Hora completa: " + dataHoraAtual.ToString("HH:mm:ss"));
+                break;
+            case 3:
+                Console.WriteLine("Data e Hora: " + dataHoraAtual.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+                break;
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+
+        Console.WriteLine("Clique qualquer botão para encerrar...");
+        Console.ReadKey();
+    }
+}
