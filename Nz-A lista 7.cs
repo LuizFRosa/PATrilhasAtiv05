212a17b798519d5bd11015499882980e260ad33d
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-05 10:25:37 -0300

    Nz-A lista 7.cs

---
+++ Nz-A lista 7.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+            Console.Write("Digite um número inteiro positivo: ");
+        int numero;
+
+        while (!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
+        {
+            Console.WriteLine("Por favor, insira um número inteiro positivo.");
+        }
+            Console.WriteLine("Números pares entre 0 e " + numero + ":");
+
+        for (int i = 0; i <= numero; i += 2)
+        {
+           Console.WriteLine("Clique qualquer botão para encerrar...");
+           Console.ReadKey();
+        }
+    }
+}
