d7b23d97326acff5520513f3ffbf1b6da5c3c542
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-02-06 08:57:33 -0300

    Nz-D lista 7.cs

---
+++ Nz-D lista 7.cs
@@ -0,0 +1,34 @@
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+              Console.WriteLine("digite uma senha: ");
+              string senha =Console.ReadLine();
+        
+              int tentativas = 0;
+              int maxTentativas = 3;
+        
+            while (tentativas < maxTentativas )
+                {
+                Console.WriteLine("insira a senha: "); 
+            
+                string tentativaSenha = (Console.ReadLine());
+            
+            if (tentativaSenha == senha)
+            {
+                Console.WriteLine("A senha esta correta");
+                Console.ReadKey();
+                break; 
+            }
+            else
+            {
+                Console.WriteLine("a senha esta incorreta.");
+                tentativas++;
+            }
+                Console.WriteLine("Clique qualquer botão para encerrar...");
+                Console.ReadKey();
+    }
+}
